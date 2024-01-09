964b57403f505fa28fc3c551b31274b7cd5057cf
BlitzMK <sec180005@utdallas.edu>
2024-01-08 18:40:05 -0600

    first functional

--- Source/NotionInterface.py
+++ Source/NotionInterface.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 
 NOTION_TOKEN = "secret_KDArq1ZKPiyQjU2DLxTUia1WxIgLVkeK81pbaVE9W9R"
 NOTION_PAGE_ID_MAIN = "Main-b44b973ac289441a888528f47135a0ae"
-DATABASE_ID = "ede5454f3b0e428ebc97ff6169b00db6?v=e56e816bac9a4b7f95a61ad0e738cce0&pvs=4"
+DATABASE_ID = "ede5454f3b0e428ebc97ff6169b00db6"
 
 headers = {
     "Authorization": "Bearer "+ NOTION_TOKEN,
@@ -17,3 +17,24 @@ def create_page():
 def get_pages(num_Pages = None):
     
     url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+    payload = {"page_size": 100}
+    response  = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+
+    import json
+    with open('db.json','w',encoding='utf8') as f:
+        json.dump(data, f,ensure_ascii=False, indent=4)
+
+    results = data["results"]
+    return results
+
+pages = get_pages()
+for page in pages:
+    page_id = page["id"]
+    props = page["properties"]
+    url = props["URL"]["title"][0]["text"]["content"]
+    title = props["Title"]["rich_text"][0]["text"]["content"]
+    published = props["Published"]["date"]["start"]
+    published = datetime.fromisoformat(published)
+    print(url,title,published)
